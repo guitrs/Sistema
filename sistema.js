f60937b2e406e276a9a1f2025050c8f33c8f75a0
guitrs <guitrs@gmail.com>
2024-11-22 20:42:28 -0300

    adicionado informação da etapa 2

--- sistema.js
+++ sistema.js
@@ -7,3 +7,14 @@ if(peso < 100){
 }else{
  console.log("A peça possui peso adequado")   
 }
+
+
+//Dada a capacidade de cada caixa, caso a lista de peças seja superior a 10, imprimir uma mensagem informando não ter capacidade suficiente.
+
+let listadePecas = ["Amortecedor", "Disco de Freio", "Motor"]
+
+if(lisatadePecas.length < 10){
+    console.log("É possivel cadastrar mais peça")
+}else{
+    console.log("Não tem mais espaço na lista")
+}
