a4dcd6b997e6a5a4ecfa83e8008f605f3bddb090
guitrs <guitrs@gmail.com>
2024-11-22 20:35:46 -0300

    adicionado informações da etapa 1

--- sistema.js
+++ sistema.js
@@ -1,1 +1,9 @@
 //Se a peça possuir um peso superir a 100 gramas, pode cadastrar
+
+let peso = 50;
+
+if(peso < 100){
+ console.log("A peça precisa pesar no mínim 100g")
+}else{
+ console.log("A peça possui peso adequado")   
+}
