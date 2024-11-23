36863ed6b98e4c54b491715553e5ca0e2281a187
guitrs <guitrs@gmail.com>
2024-11-22 22:17:21 -0300

    adicionando comandos etapa 2

--- sistemaaluno.js
+++ sistemaaluno.js
@@ -6,3 +6,10 @@ for (let index = 0; index < numeroDeAlunos; index++){
     console.log(index)
 
 }
+
+
+//Se o número for zero, escreva 'zero' e o número correspondente.
+
+if(index == 0){
+        console.log("O número atual é zero")
+}
