7a0b40bc752badd39c50af8e110d772d72dda033
guitrs <guitrs@gmail.com>
2024-11-22 22:24:42 -0300

    adicionando etapa clear

--- sistemaaluno.js
+++ sistemaaluno.js
@@ -5,11 +5,20 @@ let numeroDeAlunos = 10;
 for (let index = 0; index < numeroDeAlunos; index++){
     console.log(index)
 
-}
+
 
 
 //Se o número for zero, escreva 'zero' e o número correspondente.
 
-if(index == 0){
+    if(index == 0){
         console.log("O número atual é zero")
 }
+
+//Se o numero for par, escreva 'par' e o número correspondente.
+
+    }else if(index %2 == 0){
+        console.log("O número é par")
+
+
+
+    }
