2f84d222caee6077fa53093364e4a780ad23cfb4
guitrs <guitrs@gmail.com>
2024-11-22 22:28:07 -0300

    adicionando comando da etapa 4

--- sistemaaluno.js
+++ sistemaaluno.js
@@ -6,8 +6,6 @@ for (let index = 0; index < numeroDeAlunos; index++){
     console.log(index)
 
 
-
-
 //Se o número for zero, escreva 'zero' e o número correspondente.
 
     if(index == 0){
@@ -19,6 +17,11 @@ for (let index = 0; index < numeroDeAlunos; index++){
     }else if(index %2 == 0){
         console.log("O número é par")
 
+//Se o número for ímpar, escreva 'ímpar' e o número correspondente.
 
+
+    }else{
+        console.log("O número é ímpar")
+    }
 
     }
